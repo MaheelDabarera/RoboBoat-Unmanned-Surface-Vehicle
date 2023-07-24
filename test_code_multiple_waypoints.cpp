60c08bc183110b1ced1d3497cc1ca5754b35af0c
Maheel Dabarera <maheel.m6@gmail.com>
2023-07-25 00:45:14 +0200

    Update test_code_multiple_waypoints.cpp
    Updated C++ File for the corresponding Arduino Code

--- test_code_multiple_waypoints.cpp
+++ test_code_multiple_waypoints.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 // Function prototypes
-double distance_between(double lat1, double long1, double lat2, double long2);
-void get_alpha(double X, double Y, double waypoint_lat, double waypoint_lon, double& alpha);
-void get_beta(double alpha, double theta, double& beta);
-void go_to_waypoint(double X, double Y, double theta, double waypoint_lat, double waypoint_lon);
-void navigate_to_waypoints(const vector<pair<double, double>>& waypoints);
+double distanceBetween(double lat1, double long1, double lat2, double long2);
+void getAlpha(double X, double Y, double waypointLat, double waypointLon, double& alpha);
+void getBeta(double alpha, double theta, double& beta);
+void goToWaypoint(double X, double Y, double theta, double waypointLat, double waypointLon);
+void navigateToWaypoints(const vector<pair<double, double>>& waypoints);
 
 // Constants
 const double pi = 3.141592;
@@ -24,61 +24,89 @@ const double MOTOR_PID_KI = 0;
 const double MOTOR_PID_KD = 0;
 
 int main() {
-  vector<pair<double, double>> waypoints = {
-      {14.078877, 100.614978}, // First waypoint
-      {14.078877, 100.614978}  // Change second waypoint
-  };
+    vector<pair<double, double>> waypoints = {
+        {14.078877, 100.614978}, // First waypoint
+        {14.078877, 100.614978}  // Change second waypoint
+    };
 
-  navigate_to_waypoints(waypoints);
+    navigateToWaypoints(waypoints);
 
-  cout << "Reached all waypoints." << endl;
+    cout << "Reached all waypoints." << endl;
 
-  return 0;
+    return 0;
 }
 
-double distance_between(double lat1, double long1, double lat2, double long2) {
-  double delta_lat = radians(lat2 - lat1);
-  double delta_long = radians(long2 - long1);
+double distanceBetween(double lat1, double long1, double lat2, double long2) {
+    double deltaLat = radians(lat2 - lat1);
+    double deltaLong = radians(long2 - long1);
 
-  double a = sin(delta_lat / 2) * sin(delta_lat / 2) +
-           cos(radians(lat1)) * cos(radians(lat2)) *
-           sin(delta_long / 2) * sin(delta_long / 2);
+    double a = sin(deltaLat / 2) * sin(deltaLat / 2) +
+               cos(radians(lat1)) * cos(radians(lat2)) *
+               sin(deltaLong / 2) * sin(deltaLong / 2);
 
-  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
 
-  return EARTH_RADIUS * c;
+    return EARTH_RADIUS * c;
 }
 
-void get_alpha(double X, double Y, double waypoint_lat, double waypoint_lon, double& alpha) {
-  alpha = atan2((X - waypoint_lat), (waypoint_lon - Y));
-  alpha = (360 / (2 * pi)) * alpha;
-  alpha = alpha + 90; // Measure from north.
+void getAlpha(double X, double Y, double waypointLat, double waypointLon, double& alpha) {
+    alpha = atan2((X - waypointLat), (waypointLon - Y));
+    alpha = (360 / (2 * pi)) * alpha;
+    alpha = alpha + 90; // Measure from north.
 
-  if (alpha < 0) alpha += 360; // Make it in the 0 - 360 range.
+    if (alpha < 0) alpha += 360; // Make it in the 0 - 360 range.
 }
 
-void get_beta(double alpha, double theta, double& beta) {
-  beta = alpha - theta;
+void getBeta(double alpha, double theta, double& beta) {
+    beta = alpha - theta;
 }
 
-void go_to_waypoint(double X, double Y, double theta, double waypoint_lat, double waypoint_lon) {
-  double alpha, beta, distance;
+void goToWaypoint(double X, double Y, double theta, double waypointLat, double waypointLon) {
+    double alpha, beta, distance;
 
-  do {
-    // Calculate alpha and beta
-    get_alpha(X, Y, waypoint_lat, waypoint_lon, alpha);
-    get_beta(alpha, theta, beta);
+    do {
+        // Calculate alpha and beta
+        getAlpha(X, Y, waypointLat, waypointLon, alpha);
+        getBeta(alpha, theta, beta);
 
-    // Compute PID output and adjust motor speed
-    double motor_speed = MOTOR_SPEED_BASE + MOTOR_PID_KP * beta;
-    motor_speed = max(motor_speed, MOTOR_SPEED_MIN);
-    motor_speed = min(motor_speed, MOTOR_SPEED_MAX);
+        // Compute PID output and adjust motor speed
+        // ... (Add PID and motor control logic here)
 
-    // Update boat's position and heading (You need to implement these functions)
-    // ... (Update X, Y, and theta based on boat's GPS and compass data)
+        // Calculate new distance to the waypoint
+        distance = distanceBetween(X, Y, waypointLat, waypointLon);
 
-    distance = distance_between(X, Y, waypoint_lat, waypoint_lon);
-  } while (distance >= WAYPOINT_REACHED_THRESHOLD);
+        // Update boat's position and heading (You need to implement these functions)
+        // ... (Update X, Y, and theta based on boat's GPS and compass data)
 
-  // Stop the boat when the waypoint is reached
-  // ... (Add boat stopping logic here)
+    } while (distance >= WAYPOINT_REACHED_THRESHOLD);
+
+    // Stop the boat when the waypoint is reached
+    // ... (Add boat stopping logic here)
+}
+
+void navigateToWaypoints(const vector<pair<double, double>>& waypoints) {
+    for (const auto& waypoint : waypoints) {
+        double waypointLat = waypoint.first;
+        double waypointLon = waypoint.second;
+
+        // Wait until GPS is locking (You need to implement this function)
+        // ... (Wait until GPS data is available)
+
+        while (true) {
+            // Update boat's position and heading (You need to implement these functions)
+            // ... (Update X, Y, and theta based on boat's GPS and compass data)
+
+            // Go to the current waypoint
+            goToWaypoint(X, Y, theta, waypointLat, waypointLon);
+
+            // Check if the boat is near the waypoint
+            double distance = distanceBetween(X, Y, waypointLat, waypointLon);
+            if (distance < WAYPOINT_REACHED_THRESHOLD) {
+                break; // Move to the next waypoint
+            }
+
+            // Add a small delay between iterations (You can adjust this delay as needed)
+            // ... (Add delay here if needed)
+        }
+    }
+}
